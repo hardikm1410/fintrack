9b7ee8a01f0345bf99485a0f7122433fc8e16e2d
hardikm1410 <hardik.baps.ahd@gmail.com>
2025-03-07 21:38:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
-const navLinks = document.querySelector('.nav-links');
+
 const body = document.querySelector('body');
 
 
