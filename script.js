ae5f88e48b9cd0ddf5de58fe6a4e153e1eea9a1e
hardikm1410 <hardik.baps.ahd@gmail.com>
2025-03-08 11:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
 
-hamburgerMenu.addEventListener('click', (e) => {
+hamburgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-nav-active);
 });
 
