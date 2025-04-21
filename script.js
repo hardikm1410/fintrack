57965fe44f2d2e90cbb7afa1e8e3e987041b1a9e
hardikm1410 <hardik.baps.ahd@gmail.com>
2025-04-21 13:57:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- Document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 anchor.addEventListener('click', function(e) {
     e.preventDefault();
 
